74e7ee7d09d5e14ed48fdffdff7367aa7f27f36a
William Haslag <wihas@smile.fr>
2019-11-18 17:00:14 +0100

    modified firebase utils, added code to store and create users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:9507927179:web:c3a8ac4126fe6ec877508e"
 };
 
+export const createUserProfileDocument = async (userAuth, additonalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt =  new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additonalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 //export firebase function methods
