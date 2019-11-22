b1d1a7868315c19c14555324f0c5ad14b61c9760
William Haslag <wihas@smile.fr>
2019-11-22 15:55:40 +0100

    moved middleware redux-logger, applies only when in dev environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,14 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+//inside Node there's an env variable, we can set it or heroku can set it
+//create-react-app sets this variable for us which we access with .env
+//It's either development or test
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
