b42b27d311471108119ee458171697c0ef5a4a71
William Haslag <wihas@smile.fr>
2020-05-25 08:46:32 +0200

    added stripe route to node.js server

---
+++ client/src/redux/store.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+//inside Node there's an env variable, we can set it or heroku can set it
+//create-react-app sets this variable for us which we access with .env
+//It's either development or test
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
